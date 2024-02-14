725a2a8f9ed788f5511d403e88122c04c1c17ba5
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-02-14 13:18:22 +0800

    Implement profile tags

---
+++ src/components/ProfileTag.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class = "profile__tag">
+        <span>{{ label }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileTag',
+    props: {
+        label: { required: true, type: String },
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.profile__tag {
+    display: flex;
+    padding: 8px 16px;
+    align-items: flex-start;
+    
+    border-radius: 32px;
+    background: var(--Blue-700);
+}
+
+.profile__tag > span {
+    color: var(--Gray-50);
+    text-align: center;
+    font-size: 1.2rem;
+    font-weight: 400;
+    text-transform: uppercase;
+}
+</style>
