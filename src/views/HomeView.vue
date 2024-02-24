fd0c85b691e11aea20ded41cff0e38ec2d74b2c5
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-02-24 15:41:56 +0800

    Move portfolio template to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,66 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppHeader></AppHeader>
+  <div class = "container">
+    <div class = "overflow_hidden">
+      <div class= "padding_restrict">
+        <div class = "workable_area profile__body">
+          <AppCardGroup  v-for = "group in CardGroups" :key = "group.id" :label = "group.label" :content = "group.content"></AppCardGroup>
+        </div>
+      </div>
+    </div>
   </div>
+  <AppFooter></AppFooter>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppHeader from '../components/layout/AppHeader.vue'
+import AppFooter from '../components/layout/AppFooter.vue'
+import AppCardGroup from '../components/ui/AppCardGroup.vue'
+import uniqueId from "lodash.uniqueid";
+
+import '@splidejs/vue-splide/css/core';
 
 export default {
-  name: 'HomeView',
+  name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AppFooter,
+    AppCardGroup
+  },
+  data() {
+    return {
+      CardGroups: [
+      {
+        id: uniqueId("card_group-"),
+        label: "Development",
+        content: [
+        { id: uniqueId("card-"), name: "Coins", description: "Your Budget Tracking Companion", done: false },
+        { id: uniqueId("card-"), name: "ACRVSL", description: "Reusable Web Components", done: false },
+        { id: uniqueId("card-"), name: "meSHM", description: "All-in-one Management Facility", done: true },
+        { id: uniqueId("card-"), name: "MVISion", description: "Secure and Scalable API", done: true },
+        { id: uniqueId("card-"), name: "Axie Finder", description: "A Discord Bot for Finding Axies", done: true },
+        ],
+      },
+      {
+        id: uniqueId("card_group-"),
+        label: "Design",
+        content: [
+        { id: uniqueId("card-"), name: "Delivery²", description: "Case Study", done: true },
+        { id: uniqueId("card-"), name: "Kada", description: "Case Study", done: true },
+        ],
+      },
+      ]
+      
+    }
   }
 }
 </script>
+
+<style>
+.profile__body {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 64px;
+}
+</style>
