74dcfe5527c5cb165c80106703878432b2422696
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-02-24 15:36:52 +0800

    Install Vue Router

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,30 @@
 <template>
-  <AppHeader></AppHeader>
-  <div class = "container">
-    <div class = "overflow_hidden">
-      <div class= "padding_restrict">
-        <div class = "workable_area profile__body">
-          <AppCardGroup  v-for = "group in CardGroups" :key = "group.id" :label = "group.label" :content = "group.content"></AppCardGroup>
-        </div>
-      </div>
-    </div>
-  </div>
-  <AppFooter></AppFooter>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script>
-import AppHeader from './components/layout/AppHeader.vue'
-import AppFooter from './components/layout/AppFooter.vue'
-import AppCardGroup from './components/ui/AppCardGroup.vue'
-import uniqueId from "lodash.uniqueid";
-
-import '@splidejs/vue-splide/css/core';
-
-export default {
-  name: 'App',
-  components: {
-    AppHeader,
-    AppFooter,
-    AppCardGroup
-  },
-  data() {
-    return {
-      CardGroups: [
-      {
-        id: uniqueId("card_group-"),
-        label: "Development",
-        content: [
-        { id: uniqueId("card-"), name: "Coins", description: "Your Budget Tracking Companion", done: false },
-        { id: uniqueId("card-"), name: "ACRVSL", description: "Reusable Web Components", done: false },
-        { id: uniqueId("card-"), name: "meSHM", description: "All-in-one Management Facility", done: true },
-        { id: uniqueId("card-"), name: "MVISion", description: "Secure and Scalable API", done: true },
-        { id: uniqueId("card-"), name: "Axie Finder", description: "A Discord Bot for Finding Axies", done: true },
-        ],
-      },
-      {
-        id: uniqueId("card_group-"),
-        label: "Design",
-        content: [
-        { id: uniqueId("card-"), name: "Delivery²", description: "Case Study", done: true },
-        { id: uniqueId("card-"), name: "Kada", description: "Case Study", done: true },
-        ],
-      },
-      ]
-      
-    }
-  }
-}
-</script>
-
 <style>
-.profile__body {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 64px;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import VueSplide from '@splidejs/vue-splide';
+import router from './router'
 
-const app = createApp( App );
+const app = createApp( App ).use(router);
 app.use( VueSplide );
 app.mount( 'body' );
