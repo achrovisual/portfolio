37f32d5239b69a2a672428ae0061bc3ecf44c755
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-02-24 16:30:04 +0800

    Apply navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <AppNavBar v-for = "item in Pages" :key = "item.id" :pages = "item.content"></AppNavBar>
   <router-view/>
 </template>
+<script>
+import AppNavBar from './components/ui/AppNavBar.vue';
+import uniqueId from "lodash.uniqueid";
 
+export default {
+  name: 'App',
+  components: {
+    AppNavBar
+  },
+  data() {
+    return {
+      Pages: [
+      {
+        id: uniqueId("pages-"),
+        content: [
+        { id: uniqueId("page-"), label: "Work", url: "/" },
+        { id: uniqueId("page-"), label: "Photography", url: "/about" }
+        ]
+      },
+      ]
+    }
+  }
+}
+</script>
 <style>
 </style>
