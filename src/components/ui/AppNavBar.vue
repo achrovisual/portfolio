5da7bf85dc1d8eef450d64587b68e48c5afe703b
Eugenio Pastoral <pastoraleugenio@gmail.com>
2024-02-24 16:29:51 +0800

    Implement navigation bar

---
+++ src/components/ui/AppNavBar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class = "navbar__container">
+        <AppNavBarLink v-for = "item in pages" :key = "item.id" :label = "item.label" :url = "item.url"></AppNavBarLink>
+    </div>
+</template>
+
+<script>
+import AppNavBarLink from './AppNavBarLink.vue'
+
+export default {
+    name: 'AppNavBar',
+    props: {
+        pages: Array
+    },
+    components: {
+        AppNavBarLink
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar__container {
+    width: fit-content;
+    display: flex;
+    padding: 4px;
+    align-items: flex-start;
+    gap: 4px;
+
+    position: fixed;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 82px;
+    z-index: 1;
+    
+    border-radius: 32px;
+    background: var(--Gray-900);
+    box-shadow: 0px 0px 8px 2px rgba(0, 0, 0, 0.10);
+    backdrop-filter: blur(50px);
+}
+</style>
---
+++ src/components/ui/AppNavBarLink.vue
@@ -0,0 +1,42 @@
+<template>
+    <router-link :to = "url" class = "navbar__link-container">
+        <span>
+            {{ label }}
+        </span>
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'AppNavBarLink',
+    props: {
+        label: { required: true, type: String },
+        url: { required: true, type: String },
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.navbar__link-container {
+    display: flex;
+    padding: 12px 16px;
+    align-items: flex-start;
+    
+    border-radius: 32px;
+
+    text-decoration: none;
+}
+
+.navbar__link-container > span {
+    color: var(--Gray-50);
+    text-align: center;
+    font-size: 1.3rem;
+    font-weight: 400;
+}
+
+.navbar__link-container:hover, .router-link-active, .router-link-exact-active {
+    border-radius: 32px;
+    background: var(--Gray-800);
+}
+</style>
