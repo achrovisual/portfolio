56e9c5bd177865832be76a32452996b2b4f6744e
Eugenio Pastoral <pastoraleugenio@gmail.com>
2025-07-02 10:31:19 +0000

    Add Git Feed to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
 
 import Gallery from "../components/Gallery";
 import { fetchGalleryData, GalleryItem } from "../api/gallery";
+import GitFeed from "../components/GitFeed";
 
 export default async function Home() {
   let myGalleryData: GalleryItem[] = [];
@@ -20,17 +21,29 @@ export default async function Home() {
 
   return (
     <div className="flex flex-grow flex-col">
-      {errorFetchingData ? (
-        <p className="text-center text-red-500 mt-8">
-          Failed to load gallery images. Please try again later.
-        </p>
-      ) : myGalleryData.length > 0 ? (
-        <Gallery galleryItemsData={myGalleryData} />
-      ) : (
-        <p className="text-center text-gray-500 mt-8">
-          No gallery items found.
-        </p>
-      )}
+      <div className="flex flex-grow flex-col min-h-screen items-center justify-center">
+        {errorFetchingData ? (
+          <p className="text-center text-red-500 mt-8">
+            Failed to load gallery images. Please try again later.
+          </p>
+        ) : myGalleryData.length > 0 ? (
+          <Gallery galleryItemsData={myGalleryData} />
+        ) : (
+          <p className="text-center text-gray-500 mt-8">
+            No gallery items found.
+          </p>
+        )}
+      </div>
+      <div className="mx-auto px-4 pb-4 flex-grow-0 h-[300px] flex flex-col">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 flex-grow overflow-hidden">
+          <div className="bg-white p-6 rounded-4xl shadow-md flex flex-col items-center justify-center overflow-y-auto">
+            <GitFeed />
+          </div>
+          <div className="bg-white p-6 rounded-4xl shadow-md flex flex-col items-center justify-center overflow-y-auto">
+            <p className="text-gray-600 text-center">placeholder</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
