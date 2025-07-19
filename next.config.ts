420786f48175c7a513ae639b33454501ca553289
Eugenio Pastoral <gino@achrovisual.com>
2025-07-19 04:13:58 +0000

    Configure serverComponentsExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: [
+    "uuid",
+    "pino",
+    "pino-pretty",
+    "react-device-detect",
+  ],
 };
 
 export default nextConfig;
