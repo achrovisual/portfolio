c81b5b6de0f4aa3d3e355d14635b88a6617d920a
Eugenio Pastoral <gino@achrovisual.com>
2025-07-27 09:57:38 +0000

    Make navigation options configurable

--- src/components/FooterNavbar.tsx
+++ src/components/FooterNavbar.tsx
@@ -1,35 +1,33 @@
-// components/FooterNavbar.tsx
 import React from "react";
-import NavLink from "./NavLink"; // Import the new NavLink component
+import NavLink from "./NavLink";
 
 interface FooterNavbarProps {
   className?: string;
   activePage: string;
   setActivePage: (page: string) => void;
+  options?: Array<{ key: string; label: string }>;
 }
 
 const FooterNavbar: React.FC<FooterNavbarProps> = ({
   className,
   activePage,
   setActivePage,
+  options = [
+    { key: "photos", label: "Photos" },
+    { key: "info", label: "Info" },
+    { key: "works", label: "Works" },
+  ],
 }) => {
   return (
     <div className={`gap-2 ${className} flex justify-center py-4`}>
-      <NavLink
-        label="Photos"
-        isActive={activePage === "photos"}
-        onClick={() => setActivePage("photos")}
-      />
-      <NavLink
-        label="Info"
-        isActive={activePage === "info"}
-        onClick={() => setActivePage("info")}
-      />
-      <NavLink
-        label="Works"
-        isActive={activePage === "works"}
-        onClick={() => setActivePage("works")}
-      />
+      {options.map(({ key, label }) => (
+        <NavLink
+          key={key}
+          label={label}
+          isActive={activePage === key}
+          onClick={() => setActivePage(key)}
+        />
+      ))}
     </div>
   );
 };
